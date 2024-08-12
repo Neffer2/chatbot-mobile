039114191cb104050850db2aa93b8f62378eb3c2
Neffer2 <n3f73r@gmail.com>
2024-08-12 17:45:25 -0500

    first commit

---
+++ database/migrations/2024_08_12_204130_create_visitas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visitas', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('user_id');
+            $table->foreign('pdv_id')->references('id')->on('puntos_venta');
+            $table->foreignId('pdv_id');
+            $table->string('foto_pdv');
+            $table->string('segmento');
+            $table->string('registro_punto')->nullable();
+            $table->string('terpel')->nullable();
+            $table->string('mobil')->nullable();
+            $table->string('foto_fatura')->nullable();
+
+            $table->foreign('estado_id')->references('id')->on('estados');
+            $table->foreignId('estado_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visitas');
+    }
+};
