981ae69bd50120c47cd38be8b6d112cd8c042f06
Neffer2 <n3f73r@gmail.com>
2024-08-13 11:35:39 -0500

    1. Pruena insert

--- app/Http/Controllers/visitasController.php
+++ app/Http/Controllers/visitasController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Visita;
 
 class visitasController extends Controller
 {
     public function insert(Request $request){
+        $visita = new Visita;
+
+        $visita->user_id = 1;
+        $visita->pdv_id = 1;
+        $visita->foto_pdv = $request->foto_pdv;
+        $visita->segmento = $request->segmento;
+        $visita->registro_punto = $request->confirma_inscrito;
+        $visita->terpel = $request->confirma_terpel;
+        $visita->mobil = $request->confirma_mobil;
+        $visita->foto_fatura = $request->foto_factura;
+        $visita->estado_id = 1;
+        $visita->save();
+            
         return response()->json($request->all());
     }
 }
+ 
