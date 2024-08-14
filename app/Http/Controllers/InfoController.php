0244dc57c60eee005b34877ffc343e7eb0c87264
Neffer2 <n3f73r@gmail.com>
2024-08-13 22:49:56 -0500

    1. false x null

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -16,7 +16,7 @@ class InfoController extends Controller
 
     public function getPdv($num_pdv){
         $pdv = PuntoVenta::select('id', 'descripcion')->where('num_pdv', $num_pdv)->first();
-        $punto_inscrito = ($pdv->visitas->where('punto_inscrito', "Si.")->first()) ? true : false;
+        $punto_inscrito = ($pdv->visitas->where('punto_inscrito', "Si.")->first()) ? true : null;
         return response()->json(['id' => $pdv->id, 'descripcion' => $pdv->descripcion, 'punto_inscrito' => $punto_inscrito]);   
     }
 }
