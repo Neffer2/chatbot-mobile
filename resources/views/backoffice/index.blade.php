56ab4d45608b040af67dcb719b50e48e73f253fd
Neffer2 <n3f73r@gmail.com>
2024-08-20 16:58:19 -0500

    ?

--- resources/views/backoffice/index.blade.php
+++ resources/views/backoffice/index.blade.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    {{-- <livewire:backoffice.visitas> --}}
+    {{-- <livewire:backoffice.visitas>  --}}
+    @livewire('backoffice.visitas')
 </body>
 </html>
