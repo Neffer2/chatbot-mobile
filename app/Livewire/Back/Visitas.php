99cf60db4866241a26ca1e204798c8691c69f705
Sebastián Camargo <juansecapri@hotmail.com>
2024-09-20 12:28:36 -0500

    Ajustes y avances en pagina login

--- app/Livewire/Back/Visitas.php
+++ app/Livewire/Back/Visitas.php
@@ -7,78 +7,84 @@ use Livewire\WithPagination;
 use App\Models\Visita;
 use App\Models\User;
 use App\Models\ItemMeta;
-use App\Models\RegistroVisita; 
+use App\Models\RegistroVisita;
 
-class Visitas extends Component 
-{ 
-    use WithPagination;  
-    
+class Visitas extends Component
+{
+    use WithPagination;
+
     // Models
     public $documento;
 
     // Useful vars
     public $visitas_user = [];
-    
+
     public function render()
-    {   
+    {
         $visitas = Visita::where('estado_id', 2)->paginate(15);
         return view('livewire.back.visitas', ['visitas' => $visitas]);
-    } 
+    }
 
-    public function cambioEstado($visita_id, $estado){
+    public function cambioEstado($visita_id, $estado)
+    {
         $visita = Visita::find($visita_id);
         $visita->estado_id = $estado;
         $visita->update();
 
-        if ($visita->estado_id == 1){
+        if ($visita->estado_id == 1) {
             $this->establecerPuntos($visita);
             return redirect()->back()->with('success', 'Visita aprobada correctamente.');
-        }else {
+        } else {
             return redirect()->back()->with('success', 'Visita rechazada correctamente.');
         }
     }
 
-    public function buscar(){
+    public function buscar()
+    {
         $this->validate([
             'documento' => 'required'
         ]);
-        
+
         $this->visitas_user = User::where('documento', $this->documento)->first()->visitas->where('estado_id', 1);
     }
 
     // PUNTOS 
-    public function establecerPuntos($visita){
+    public function establecerPuntos($visita)
+    {
         /** INSCRITOS **/
-        if (is_null($visita->punto_inscrito) && ($visita->terpel != "Si." && $visita->mobil != "Si.")){
+        // if (is_null($visita->punto_inscrito) && ($visita->terpel != "Si." && $visita->mobil != "Si.")){
+        if ($visita->pdv_inscrito == "Si.") {
             // Frecuencia 
             $this->sumPuntos($visita, 1);
             //Visibilidad
             $this->sumPuntos($visita, 2);
-            
+
             $visita->estado_id_agente = 1;
             $visita->update();
         }
 
         /** NO INSCRITOS **/
-        if ($visita->punto_inscrito == "No."){
+        if ($visita->pdv_inscrito == "No.") {
             // Cobertura
             $this->sumPuntos($visita, 4);
 
             $visita->estado_id_agente = 1;
             $visita->update();
-        }  
+        }
     }
 
-    public function registroVisita($user_id, $visita_id, $item_meta_id, $puntos){
+    public function registroVisita($user_id, $visita_id, $item_meta_id, $puntos)
+    {
         $registro_visita = new RegistroVisita;
         $registro_visita->user_id = $user_id;
         $registro_visita->visita_id = $visita_id;
         $registro_visita->item_meta_id = $item_meta_id;
         $registro_visita->puntos = $puntos;
-        $registro_visita->save(); 
+        $registro_visita->save();
     }
 
-    public function sumPuntos($visita, $item){
+    public function sumPuntos($visita, $item)
+    {
         $items_metas = ItemMeta::all();
         /*
             1 - Frecuencia
