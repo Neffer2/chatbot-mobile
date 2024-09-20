07c79dcd704f89e24628732526971a2df0552a82
Neffer2 <n3f73r@gmail.com>
2024-09-20 18:06:03 -0500

    1. Avacnes Backend

--- app/Livewire/Back/Visitas.php
+++ app/Livewire/Back/Visitas.php
@@ -31,6 +31,7 @@ class Visitas extends Component
         $visita->update();
 
         if ($visita->estado_id == 1){
+        // if (true){
             $this->establecerPuntos($visita);
             return redirect()->back()->with('success', 'Visita aprobada correctamente.');
         }else {
@@ -48,25 +49,50 @@ class Visitas extends Component
 
     // PUNTOS
     public function establecerPuntos($visita){
-        /** INSCRITOS **/
+        $num_vista = Visita::where([['user_id', $visita->user_id], ['pdv_id', $visita->pdv_id]])->count();
+        $pdv_inscrito = (Visita::where([['user_id', $visita->user_id], ['pdv_id', $visita->pdv_id], ['pdv_inscrito', 'Si.']])->first()) ? true : false;
 
-        if (is_null($visita->punto_inscrito) && ($visita->terpel != "Si." && $visita->mobil != "Si.")){
-            // Frecuencia
-            $this->sumPuntos($visita, 1);
-            //Visibilidad
-            $this->sumPuntos($visita, 2);
+        // Primera visita
+        if ($num_vista == 1 && !is_null($visita->foto_factura) && $visita->pdv_inscrito == "Si."){
+            // Cobertura
+            $this->sumPuntos($visita, 4);
+            // Volumen
+            $this->sumPuntos($visita, 3);
 
-            $visita->estado_id_agente = 1;
-            $visita->update();
-        }
-
-        /** NO INSCRITOS **/
-        if ($visita->punto_inscrito == "No."){
+            return redirect()->back()->with('success', 'Visita aprobada correctamente.');
+        }elseif ($num_vista == 1 && is_null($visita->foto_factura) && $visita->pdv_inscrito == "No."){
             // Cobertura
             $this->sumPuntos($visita, 4);
 
-            $visita->estado_id_agente = 1;
-            $visita->update();
+            return redirect()->back()->with('success', 'Visita aprobada correctamente.');
+        }
+
+        // Visitas 2,3,4
+        elseif ($num_vista > 1 && !is_null($visita->foto_factura) && $pdv_inscrito){
+            // Frecuencia
+            $this->sumPuntos($visita, 1);
+            // Volumen
+            $this->sumPuntos($visita, 3);
+
+            return redirect()->back()->with('success', 'Visita aprobada correctamente.');
+        }elseif ($num_vista > 1 && !is_null($visita->foto_factura) && $visita->pdv_inscrito == "Si."){
+            // Volumen
+            $this->sumPuntos($visita, 3);
+
+            return redirect()->back()->with('success', 'Visita aprobada correctamente.');
+        }elseif ($num_vista > 1 && is_null($visita->foto_factura) && !($pdv_inscrito)){
+
+            return redirect()->back()->with('success', 'Visita aprobada correctamente.');
+        }elseif ($num_vista > 1 && is_null($visita->foto_factura) && $pdv_inscrito){
+            // Frecuencia
+            $this->sumPuntos($visita, 1);
+
+            return redirect()->back()->with('success', 'Visita aprobada correctamente.');
+        }elseif ($num_vista > 1 && !is_null($visita->foto_factura) && !($pdv_inscrito)){
+            // Volumen
+            $this->sumPuntos($visita, 3);
+
+            return redirect()->back()->with('success', 'Visita aprobada correctamente.');
         }
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,5 @@ class User extends Authenticatable
     {
         return $this->hasMany(Visita::class, 'user_id', 'id');
     }
+
 }
