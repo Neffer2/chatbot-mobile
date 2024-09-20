33968f846ddbbf4bcbf4866063674602bbe63d55
Sebastián Camargo <juansecapri@hotmail.com>
2024-09-20 17:35:50 -0500

    Avances login y redirección a vista de Asesor

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,8 @@
     <div class="login-container">
         <!-- Image Section -->
         <div class="image-section">
-            <img src="{{ asset('assets/mobil-terpel.png') }}" alt="Login Image">
+            <img src="{{ asset('assets/login-plan-choque-text.png') }}" alt="Login Image">
+
         </div>
 
         <!-- Form Section -->
@@ -13,44 +14,47 @@
             <form method="POST" action="{{ route('login') }}">
                 @csrf
 
-                <!-- Email Address -->
                 <div>
-                    <x-input-label for="email" :value="__('Email')" />
-                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    <x-text-input id="email" class="block mt-1 w-full custom-input" type="email" name="email"
+                        :value="old('email')" required autofocus autocomplete="username"
+                        placeholder="{{ __('CORREO') }}" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2 custom-error" />
                 </div>
-
+            
                 <!-- Password -->
                 <div class="mt-4">
-                    <x-input-label for="password" :value="__('Password')" />
-                    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    <x-text-input id="password" class="block mt-1 w-full custom-input" type="password" name="password"
+                        required autocomplete="current-password" placeholder="{{ __('CONTRASEÑA') }}" />
+                    <x-input-error :messages="$errors->get('password')" class="mt-2 custom-error" />
                 </div>
-
+            
                 <!-- Remember Me -->
-                <div class="block mt-4">
+                <div class="remember-me-login-cont">
                     <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                        <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        <input id="remember_me" type="checkbox" class="custom-checkbox" name="remember">
+                        <span class="custom-checkbox-text">{{ __('TRATAMIENTO DE DATOS') }}</span>
                     </label>
                 </div>
 
-                <div class="flex items-center justify-end mt-4">
-                    @if (Route::has('password.request'))
-                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <div class="">
+                    {{-- @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            href="{{ route('password.request') }}">
                             {{ __('Forgot your password?') }}
                         </a>
-                    @endif
+                    @endif --}}
 
-                    <x-primary-button class="ms-3">
-                        {{ __('Log in') }}
-                    </x-primary-button>
+                    <div class="login-btn-container">
+                        <x-primary-button class="custom-button">
+                            {{ __('INGRESAR') }}
+                        </x-primary-button>
+                    </div>
                 </div>
             </form>
         </div>
     </div>
     <div class="login-footer">
-        <p>© MOBIL | TERPEL. TODOS LOS DERECHOS RESERVADOS.</p>
+        <p>© MOBIL | TERPEL 2024. TODOS LOS DERECHOS RESERVADOS.</p>
         <img src="{{ asset('assets/mobil-terpel.png') }}" alt="">
     </div>
 </x-guest-layout>
