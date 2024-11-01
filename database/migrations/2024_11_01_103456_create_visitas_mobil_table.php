8d859638762d7ef50a23473308386cec01b8db91
Sebastián Camargo <juansecapri@hotmail.com>
2024-11-01 12:00:33 -0500

    Ajuste de nuevas migraciones

---
+++ database/migrations/2024_11_01_103456_create_visitas_mobil_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visitas_mobil', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('user_id');
+            $table->foreign('pdv_id')->references('id')->on('puntos_venta');
+            $table->foreignId('pdv_id');
+            $table->string('foto_pop')->nullable();
+            $table->string('pdv_inscrito')->nullable();
+            $table->unsignedBigInteger('marca_id');
+            $table->foreign('marca_id')->references('id')->on('marca')->onDelete('cascade');
+            $table->string('referencias')->nullable();
+            $table->string('presentaciones')->nullable();
+            $table->integer('num_cajas')->nullable();
+            $table->string('foto_factura')->nullable();
+            $table->string('foto_precios')->nullable();
+            $table->string('valor_factura')->nullable();
+            $table->string('valor_galonaje')->nullable();
+            $table->string('observaciones')->nullable();
+            $table->foreign('estado_id')->references('id')->on('estados');
+            $table->foreignId('estado_id');
+            $table->foreign('estado_id_agente')->references('id')->on('estados');
+            $table->foreignId('estado_id_agente');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visitas');
+    }
+};
---
+++ database/migrations/2024_11_01_104556_create_puntos_venta_mobil_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('puntos_venta_mobil', function (Blueprint $table) {
+            $table->id();
+            $table->string('num_pdv')->unique();
+            $table->foreign('agente')->references('id')->on('empresas');
+            $table->foreignId('agente');
+            $table->string('descripcion');
+            $table->string('nit');
+            $table->string('direccion');
+            $table->string('barrio');
+            $table->string('localidad');
+            $table->foreign('asesor_id')->references('id')->on('users');
+            $table->foreignId('asesor_id');
+            $table->string('segmento_pdv');
+            $table->float('vol_prom_mes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('puntos_venta_mobil');
+    }
+};
