50a41d0e8aaa5e0bf4a0e3154a80ac958385a7cf
Neffer2 <n3f73r@gmail.com>
2025-01-03 10:25:33 -0500

    1. Correccion request

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -142,8 +142,6 @@ class InfoController extends Controller
 
     public function redimirPremio(Request $request)
     {
-        return response()->json(['message' => $request->all()], 200);
-
         $user = User::find($request->user_id);
         if (!$user) {
             return response()->json(['Usuario no encontrado'], 404);
