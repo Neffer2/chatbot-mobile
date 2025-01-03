94bd175b8fd3e63aacc749a7edecffacfdd7a086
Neffer2 <n3f73r@gmail.com>
2025-01-03 09:52:50 -0500

    1. Validaciones redenciones

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -199,6 +199,10 @@ class InfoController extends Controller
 
     public function getPremios($user_id, $premio_id) {
         $user = User::find($user_id);
+        if (!$user) {
+            return response()->json(['Usuario o premio no encontrado'], 404);
+        }
+
         $pdv_x_user = PuntoVenta::where('asesor_id', $user_id)->count(); // Puntos de venta asignados
         $pdv_x_user += PuntoVentaMobil::where('asesor_id', $user_id)->count(); // Puntos de venta asignados
 
@@ -222,11 +226,12 @@ class InfoController extends Controller
             ['empresa_id', $user->empresa_id]
         ])->first();
 
-        $ajuste_valor_premio = $premio->puntos * ( $pdv_x_user / $promedio_pdv);
-
-        if (!$user || !$premio) {
+        if (!$premio) {
             return response()->json(['Usuario o premio no encontrado'], 404);
         }
+
+        $ajuste_valor_premio = $premio->puntos * ( $pdv_x_user / $promedio_pdv);
+
 
         if ($user->puntos < $ajuste_valor_premio) {
             return response()->json(['Puntos insuficientes'], 400);
