8a51d9d5557afc713e5c27cf25f20fc1f5ae0062
Neffer2 <n3f73r@gmail.com>
2025-01-03 10:04:05 -0500

    1. Validacion de prueba

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -199,8 +199,8 @@ class InfoController extends Controller
 
     public function getPremios($user_id, $premio_id) {
         $user = User::find($user_id);
-        if (!$user) {
-            return response()->json(['Usuario o premio no encontrado'], 404);
+        if (is_null($user)) {
+            return response()->json(['error' => 'Usuario o premio no encontrado'], 404);
         }
 
         $pdv_x_user = PuntoVenta::where('asesor_id', $user_id)->count(); // Puntos de venta asignados
