4dc695fb0b2736d5b33736d0148ce5b355c2e909
Neffer2 <n3f73r@gmail.com>
2025-01-03 09:59:20 -0500

    1. Validacion de prueba

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -199,7 +199,9 @@ class InfoController extends Controller
 
     public function getPremios($user_id, $premio_id) {
         $user = User::find($user_id);
-        if (is_null($user)) {
+        return response()->json(['user' => $user]);
+
+        if (!$user) {
             return response()->json(['Usuario o premio no encontrado'], 404);
         }
 
